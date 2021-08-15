62bfadbe1cb8e8a77fda7eddb7c5b8ab2a746352
Hakim Boyles <hak@haktronic.com>
2021-08-15 19:53:01 -0400

    implement camera control

--- game.py
+++ game.py
@@ -4,14 +4,16 @@ from pygame.locals import *
 import levels
 import player
 
-RESOLUTION = (400, 400)
 FPS = 30
 
 pygame.init()
 pygame.mixer.init()
 
-window = pygame.display.set_mode(RESOLUTION)
+window = pygame.display.set_mode((0, 0), FULLSCREEN)
 pygame.display.set_caption("Ascendio")
+logo = pygame.image.load('assets/logo.png')
+logo.set_colorkey((255, 0, 255))
+pygame.display.set_icon(logo)
 
 clock = pygame.time.Clock()
 pygame.key.set_repeat(FPS)
@@ -29,48 +31,69 @@ playable = player.Player()
 entities.add(playable)
 
 
+def move_camera():
+
+    if playable.rect.right >= pygame.display.get_surface().get_size()[0] / 2:
+
+        if playable.acc.x > 0:
+
+            playable.pos.x -= int(abs(playable.speed * playable.acc.x))
+
+            for tile in tiles:
+                tile.rect.x -= int(abs(playable.speed * playable.acc.x))
+
+    elif playable.rect.left < pygame.display.get_surface().get_size()[0] / 3:
+
+        if playable.acc.x < 0:
+
+            playable.pos.x += int(abs(playable.speed * playable.acc.x))
+
+            for tile in tiles:
+                tile.rect.x += int(abs(playable.speed * playable.acc.x))
+
+
 def update():
     tiles.update()
     entities.update()
-    
     collided_x = False
+    move_camera()
 
     # Player collision goes here
-    if playable.rect.bottom > RESOLUTION[1]:
-        playable.rect.bottom = RESOLUTION[1]
+    if playable.rect.bottom > pygame.display.get_surface().get_size()[1]:
+        playable.rect.bottom = pygame.display.get_surface().get_size()[1]
         playable.acc.y = 0
-        
 
     for ti in levels.tiles:
 
         # Put X Axis Collision Here
-        if ti.rect.colliderect(playable.rect.x + playable.acc.x, playable.rect.y, playable.width, playable.height):
+        if ti.rect.colliderect(playable.rect.x, playable.rect.y, playable.width, playable.height):
 
-            if playable.vel.x > 0 and ti.rect.top < playable.rect.top:
-                playable.image.fill((0,0,255))
+            if playable.acc.x > 0 and ti.rect.top < playable.rect.top:
+                playable.image.fill((0, 0, 255))
                 collided_x = True
-                playable.pos.x = playable.uncollided.x - 1
-                playable.vel.x = 0
-            elif playable.vel.x < 0 and ti.rect.top < playable.rect.top:
-                playable.image.fill((0,255,0))
+                playable.pos.x = playable.uncollided.x
+                playable.pos.x -= abs(playable.speed * playable.acc.x)
+            elif playable.acc.x < 0 and ti.rect.top < playable.rect.top:
+                playable.image.fill((0, 255, 0))
                 collided_x = True
-                playable.pos.x = playable.uncollided.x + 1
-                playable.vel.x = 0
-                    
-        if ti.rect.colliderect(playable.rect.x, playable.rect.y + (playable.acc.y * 1.6), playable.width, playable.height):
+                playable.pos.x = playable.uncollided.x
+                playable.pos.x += abs(playable.speed * playable.acc.x)
+
+        if ti.rect.colliderect(playable.rect.x, playable.rect.y, playable.width,
+                               playable.height):
 
             # Y-axis Collision
             if playable.vel.y < 0 and ti.rect.bottom < playable.rect.bottom:
-                    playable.pos.y = ti.rect.bottom + playable.height
-                    playable.vel.y = 0
+                playable.pos.y = ti.rect.bottom + playable.height
+                playable.vel.y = 0
 
             elif playable.vel.y >= 0 and ti.rect.top > playable.rect.top:
-                    playable.pos.y = ti.rect.top
-                    playable.vel.y = 0
-                    
+                playable.pos.y = ti.rect.top
+                playable.vel.y = 0
+
             playable.rect.midbottom = playable.pos
-            
-    if not collided_x: 
+
+    if not collided_x:
         playable.uncollided = playable.pos
 
 
@@ -91,12 +114,11 @@ while running:
 
         if event.type == KEYDOWN:
             if event.key == pygame.K_SPACE:
-
                 # Tests if the player is standing on solid ground
                 playable.rect.y += 1
                 hits = pygame.sprite.spritecollide(playable, tiles, False)
                 playable.rect.y -= 1
-                playable.jump(hits)
+                playable.jump(hits and (playable.rect.left > hits[0].rect.left or playable.rect.right < hits[0].rect.right))
 
     update()
 
--- player.py
+++ player.py
@@ -13,18 +13,18 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.width = 20
-        self.height = 20
+        self.width = 40
+        self.height = 40
         self.original_image = pygame.Surface((self.width, self.height))
         self.original_image.fill((255, 0, 0))
         self.image = self.original_image
         self.rect = self.image.get_rect()
-        self.rect.center = vec(self.width / 2, self.height /2)
         self.pos = vec(0, 0)
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
-        # self.hits = []
+        self.speed = 8
         self.uncollided = self.pos
+
     def jump(self, able):
         if able:
             self.vel.y = -18
@@ -35,13 +35,13 @@ class Player(pygame.sprite.Sprite):
         key_state = pygame.key.get_pressed()
 
         if key_state[pygame.K_a]:
-            self.acc.x = -0.5
+            self.acc.x = -0.55
 
         elif key_state[pygame.K_d]:
-            self.acc.x = 0.5
+            self.acc.x = 0.55
 
-        self.acc.x += self.vel.x * friction
         self.vel += self.acc
-        self.pos += self.vel + 0.5 * self.acc
+        self.pos.x += self.acc.x * self.speed
+        self.pos.y += self.vel.y + 0.5 * self.acc.y
 
         self.rect.midbottom = self.pos
